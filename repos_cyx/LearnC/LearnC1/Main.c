e1ea9f08b52399d7fa2dc75cea0e27790706577d
cyxw0w <gkfn1027@gmail.com>
2020-01-18 13:01:57 +0900

    add

--- repos_cyx/LearnC/LearnC1/Main.c
+++ repos_cyx/LearnC/LearnC1/Main.c
@@ -2,6 +2,18 @@
 
 #include<Windows.h>		//수업용 헤더.
 
+enum eTrabe {
+	야수	=0,
+	언데드,
+	악마,
+};
+enum eType {
+	검 = 0,
+	도,
+	창,
+	지팡이
+};
+
 int main(void) {//실행시 제일 먼저 확인(명령어들)
 	
 				/*
@@ -48,15 +60,78 @@ int main(void) {//실행시 제일 먼저 확인(명령어들)
 	printf("입력하신 숫자 + 11는 [%d]입니다.\n", number+11);
 	*/
 
+	/*
 	char szStr[100] = { 0, };
 	int number;
 	printf("이름과 나이를 입력하세요(이름 나이) : ");
 	scanf_s("%s %d", szStr, sizeof(szStr), &number);
 	printf("당신의 이름은 [%s]님 입니다. \n나이는 %d세 입니다.", szStr, number);
+	*/
+	/*
+	연습문제
+	int year;
+	printf("태어난 년도를 입력하세요 : ");
+	scanf_s("%d", &year);
+	printf("당신의 나이는 %d세입니다.\n", 2020 - year + 1);
+	*/
+	/*printf("int\t:%d\n", sizeof(int));
+	printf("short\t:%d\n", sizeof(short));
+	printf("long\t:%d\n", sizeof(long));
+	printf("long long \t:%d\n", sizeof(long long));
+	printf("unsigned int\t:%d\n", sizeof(unsigned int));*/
 
+	/*int number = 0;
+	unsigned int value = number;
+	printf("number = %d, value = %d\n", number, value);
+	value = number = -1;
+	printf("number = %d, value = %u\n", number, value);*/
 
+	//기본적으로 상수표현시 double로 인식
+	/*float pi = 0.0f;
+	printf("pi = %.2f\n", pi);
+	pi = pi + 3.14f;
+	printf("\"pi = %1.2f\"\n", pi);*/
+	/*
+	char ch = 'Z';
+	char str[] = "안녕하세요.";
+	char* str2 = "안녕하세요2";
+	printf("str = %s\n", str);
+	printf("str2 = %s\n", str2);
+	str2 = "반가워요";
+	printf("str2 = %s\n", str2);
 
+	//printf("ch = %c\n", ch);
+	//ch = 112;
+	//printf("ch = %c\n", ch);
+	*/
 
+	//enum eTrabe monsterTrabe = 야수;		//0: 야수, 1:언데드, 2: 악마
+	//enum eType weaponType = 검;			//0: 검, 1: 도, 2: 창, 3: 지팡이.....
+
+	//printf("monsterTrabe = %d\n", monsterTrabe);
+	//printf("monsterTrabe = %d\n", monsterTrabe = 언데드);
+	//printf("monsterTrabe = %d\n", monsterTrabe = 악마);
+
+	//printf("weaponType = %d\n", weaponType);
+	//printf("weaponType = %d\n", weaponType = 도);
+	//printf("weaponType = %d\n", weaponType = 창);
+	//printf("weaponType = %d\n", weaponType = 지팡이);
+
+	//연습문제
+	char name[100] = { 0, };
+	float height =.0f;
+	float weight =.0f;
+
+	float bmi = .0f;
+	printf("이름을 입력하세요 :");
+	scanf_s("%s", name, 100);
+
+	printf("키와 몸무게를 입력하세요(###.# ##.#) :");
+	scanf_s("%f, %f", &height, &weight);
+	bmi = weight / ((height*0.01)*(height*0.01));
+	printf("%s님의 키는 %.1fCm이고, 몸무게는 %.1fKg입니다.\n", name, height, weight);
+	printf("%s님의 비만도는 %.2f입니다.(25이상은 비만)\n", name, bmi);
+	
 	system("pause");	//수업용 코드.
 	return 0;
 }
